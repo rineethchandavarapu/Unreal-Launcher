59a08438be1a31f7c0ad8be74fb113e788f805f9
Rineeth Chandavarapu <klennyrineeth@gmail.com>
2026-05-26 10:07:05 +0530

    Add build script for napi_build setup

---
+++ native/build.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2026 NeelFrostrain. All rights reserved.
+// Proprietary and confidential. Unauthorized copying, modification,
+// distribution, or use of this source code is strictly prohibited.
+// See LICENSE in the project root for full license terms.
+extern crate napi_build;
+
+fn main() {
+  napi_build::setup();
+}
